8e8de3bfb53fb8d3e9527cba8ee5d25295d876e2
= <=>
2023-01-01 18:29:29 -0500

    agregando el framework vue + tailwindcss

---
+++ vue-rest-api/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header >
+    <h1 class="text-center bg-green-500">texto centrado</h1>
+    <div >
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+
+   </header>
+
+   <RouterView />
+</template>
