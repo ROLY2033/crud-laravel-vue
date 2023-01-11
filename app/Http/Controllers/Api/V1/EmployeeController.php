4d90f1affc602a7a22440d4df83d297cb54f3edd
2023 <95943858+ROLY2033@users.noreply.github.com>
2023-01-10 20:23:02 -0500

    Create EmployeeController.php

---
+++ app/Http/Controllers/Api/V1/EmployeeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSkillRequest;
+use App\Http\Resources\V1\SkillCollection;
+use App\Http\Resources\V1\SkillResource;
+use App\Models\Skill;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    //
+}
